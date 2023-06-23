61cd2f5332b5e811b814933ffd49719960a55da0
Céline BONO <celine.bono@gmail.com>
2023-06-23 11:22:29 +0200

    Travail sur le thème enfant + création des pages dans WordPress
    Ajout des plugins et mise à jour.
    Ajout d'un dossier CSS contenant un fichier theme.css et des fichiers header et footer dans le thème enfant.
    Import de la police du site + ajout couleur de fond de la page de précommande dans le fichier theme.css.
    Création des 3 pages du site dans wordPress.
    Début de mises en place du contenu avec Gutenberg.

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -19,6 +19,7 @@ define( 'CHILD_THEME_PLANTY_VERSION', '1.0.0' );
 function child_enqueue_styles() {
 
 	wp_enqueue_style( 'planty-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all' );
+	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 
 }
 
